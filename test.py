9eac44756d1394fadae97dacb30f599a0b2a48f6
Luke Zhang <t116517uhn@h4huhnlogin2.uhnh4h.cluster>
2022-08-31 18:33:54 -0400

    update testing script + requirements

--- test.py
+++ test.py
@@ -3,13 +3,14 @@ import model
 import encoder
 
 if __name__ == '__main__':
-    data = deephase_data()
+    print('test')
+    data, categories = deephase_data()
     esm = model.ESM()
     num_layers = 1
     model_dim = 12
     num_heads = 3
     ff_dim = 12
-
-    print('test')
     encoder = encoder.Encoder(num_layers, model_dim, num_heads, ff_dim)
-    print(data)
+    print(data[0])
+    test_output = esm.get_representation([data[0]])
+    print(test_output)
