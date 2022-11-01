f52596b78bdbbd410acaae9a3c5c54a54866ba46
LZhang98 <luke.taotao@hotmail.com>
2022-10-31 23:16:09 -0400

    begin roc report

--- evaluate_model.py
+++ evaluate_model.py
@@ -24,33 +24,13 @@ my_model = Encoder(num_layers, model_dim, num_heads, ff_dim)
 my_model.load_state_dict(torch.load(path))
 my_model.eval()
 
-# Test 1: subsetted data (downsampled negative, no PDB*)
-
-# annotation1 = '/cluster/projects/kumargroup/luke/deephase_annotations_no_pdb.csv'
 data_dir = '/cluster/projects/kumargroup/luke/output/esm-embeddings-padded/'
-# dataset1 = CustomDataset(annotations_file=annotation1, data_dir=data_dir)
-# dataloader1 = DataLoader(dataset1, batch_size=batch_size)
-
-# correct, total = 0, 0
-# for i, data in enumerate(dataloader1):
-#     inputs, targets = data
-#     inputs = inputs.squeeze().float()
-#     targets = targets.unsqueeze(1).float()
-
-#     outputs = my_model(inputs)
-#     predicted = (outputs > 0.5).int()
-#     total += targets.size(0)
-#     correct += (predicted == targets).sum().item()
-
-# print(f'Accuracy: {correct/total}')
-
-# Test 2: full dataset
-annotation2 = '/cluster/projects/kumargroup/luke/deephase_annotations_uniq.csv'
-dataset2 = CustomDataset(annotations_file=annotation2, data_dir=data_dir)
-dataloader2 = DataLoader(dataset2, batch_size=batch_size)
+annotation = '/cluster/projects/kumargroup/luke/deephase_annotations_uniq.csv'
+dataset = CustomDataset(annotations_file=annotation, data_dir=data_dir)
+dataloader = DataLoader(dataset, batch_size=batch_size)
 
 correct, total = 0, 0
-for i, data in enumerate(dataloader2):
+for i, data in enumerate(dataloader):
     inputs, targets = data
     inputs = inputs.squeeze().float()
     targets = targets.unsqueeze(1).float()
